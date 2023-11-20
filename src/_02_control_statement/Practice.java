e8ae4df4c03deefcab1092a322126cf5a1554d24
Taehoon <mysoul98@gmail.com>
2023-11-20 18:43:29 +0900

    study: 반복문 실습

--- src/_02_control_statement/Practice.java
+++ src/_02_control_statement/Practice.java
@@ -1,7 +1,16 @@
 package _02_control_statement;
 
+import java.util.Scanner;
+
 public class Practice {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("숫자를 입력하세요!");
+        int numbers = sc.nextInt();
+
+        for (int i = 1; i <= numbers; i++) {
+            System.out.print(i + " ");
+        }
     }
 }
