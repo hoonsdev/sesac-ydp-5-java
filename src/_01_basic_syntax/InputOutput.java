eeadfa82ef8aad9bca01084ff749041e85407eba
Taehoon <mysoul98@gmail.com>
2023-11-22 18:06:33 +0900

    study: 표준 배열, ArrayList

--- src/_01_basic_syntax/InputOutput.java
+++ src/_01_basic_syntax/InputOutput.java
@@ -3,26 +3,27 @@ package _01_basic_syntax;
 import java.util.Scanner;
 
 public class InputOutput {
-    public static void main(String[] args) {
-        // 출력
-        System.out.print("안녕하세요, ");
-        System.out.println("저는 김새싹 입니다! ");
-        System.out.println("잘 부탁드립니다. ");
+  public static void main(String[] args) {
+    // 출력
+    System.out.print("안녕하세요, ");
+    System.out.println("저는 김새싹 입니다! ");
+    System.out.println("잘 부탁드립니다. ");
 
-        // 입력
-        System.out.println("공백으로 구분해서 이름, 나이, 키, 결혼여부를 입력해주세요 : ");
-        Scanner sc = new Scanner(System.in);
+    // 입력
+    System.out.println("공백으로 구분해서 이름, 나이, 키, 결혼여부를 입력해주세요 : ");
+    Scanner sc = new Scanner(System.in);
 
-        String name = sc.next(); // 공백 이전까지의 문자열을 읽음
-        int age = sc.nextInt(); // 공백 이전까지의 정수를 읽음
-        double height = sc.nextDouble(); // 공백 이전까지의 실수를 읽음
-        boolean single = sc.nextBoolean(); // 공백 이전까지의 불리언 읽음
+    String name = sc.next(); // 공백 이전까지의 문자열을 읽음
+    int age = sc.nextInt(); // 공백 이전까지의 정수를 읽음
+    double height = sc.nextDouble(); // 공백 이전까지의 실수를 읽음
+    boolean single = sc.nextBoolean(); // 공백 이전까지의 불리언 읽음
 
-        System.out.println("이름 : " + name);
-        System.out.println("나이 : " + age);
-        System.out.println("키 : " + height);
-        System.out.println("결혼여부 : " + single);
+    System.out.println("이름 : " + name);
+    System.out.println("나이 : " + age);
+    System.out.println("키 : " + height);
+    System.out.println("결혼여부 : " + single);
+    System.out.println();
 
-        sc.close(); // 스캐너 닫기
-    }
+    sc.close(); // 스캐너 닫기
+  }
 }
