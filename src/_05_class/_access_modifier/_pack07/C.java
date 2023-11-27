c74b6e874d7a6980af483beb9b2985e671233da7
Taehoon <mysoul98@gmail.com>
2023-11-27 16:23:43 +0900

    study: 클래스 상속 protected

---
+++ src/_05_class/_access_modifier/_pack07/C.java
@@ -0,0 +1,10 @@
+package _05_class._access_modifier._pack07;
+
+import _05_class._access_modifier._pack06.A;
+
+public class C {
+  // 다른 클래스에서는 접근 불가능!
+  //  A a = new A();
+  //  a.field1 = 11;
+  //  a.method1();
+}
